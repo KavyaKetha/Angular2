f4685ec62d0a5be1ce6d4285ef1fc53521bc0995
Kavya <kethakavya19@gmail.com>
2016-04-30 17:49:20 +0530

    Added: Sorting for Pagination

--- Pagination/app/main.ts
+++ Pagination/app/main.ts
@@ -1,144 +1,334 @@
 import {bootstrap} from 'angular2/platform/browser';
 
-//import {TableDemo} from 'app/table.demo';
 import {Component, EventEmitter, OnInit} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgIf, NgFor} from 'angular2/common';
 import { PAGINATION_DIRECTIVES } from 'ng2-bootstrap/components/pagination';
-//import {Table} from 'ng2-table/components/table';
-
 
 @Component({
-  selector: 'my-app',
-  templateUrl: 'partials/tableview.html',
-  directives: [ PAGINATION_DIRECTIVES, NgClass, NgIf, NgFor, CORE_DIRECTIVES, FORM_DIRECTIVES]
+    selector: 'my-app',
+    templateUrl: 'partials/tableview.html',
+    directives: [PAGINATION_DIRECTIVES, NgClass, NgIf, NgFor, CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
-export class AppComponent implements OnInit{
-  public rows:Array<any> = [];
-  public columns:Array<any> = [
-    {title: 'Name', name: 'name'},
-    {title: 'Position', name: 'position', sort: false},
-    {title: 'Office', name: 'office', sort: 'asc'},
-    {title: 'Extn.', name: 'ext', sort: 'desc'},
-    {title: 'Start date', name: 'startDate'},
-    {title: 'Salary', name: 'salary'}
-  ];
-  public page:number = 1;
-  public itemsPerPage:number = 2;
-  public maxSize:number = 5;
-  public numPages:number = 1;
-  public length:number = 0;
+export class AppComponent implements OnInit {
+    public rows: Array<any> = [];
+    public columns: Array<any> = [
+        { title: 'Name', name: 'name' },
+        { title: 'Position', name: 'position', sort: false },
+        { title: 'Office', name: 'office', sort: 'asc' },
+        { title: 'Extn.', name: 'ext', sort: 'desc' },
+        { title: 'Start date', name: 'startDate' },
+        { title: 'Salary', name: 'salary' }
+    ];
+    public page: number = 1;
+    public itemsPerPage: number = 6;
+    public maxSize: number = 5;
+    public numPages: number = 1;
+    public length: number = 0;
+    public tableData: Array<any> = [];
+    public config: any = {
+        paging: true,
+        sorting: { columns: [] },
+        filtering: { filterString: '', columnName: 'position' }
+    };
 
-  public config:any = {
-    paging: true,
-    sorting: {columns: []},
-    filtering: {filterString: '', columnName: 'position'}
-  };
+    public data: Array<any> = [{
+        'name': 'Victoria Cantrell',
+        'position': 'Integer Corporation',
+        'office': 'Croatia',
+        'ext': '0839',
+        'startDate': '2015/08/19',
+        'salary': '$208,178'
+    }, {
+            'name': 'Pearl Crosby',
+            'position': 'In PC',
+            'office': 'Cambodia',
+            'ext': '8262',
+            'startDate': '2014/10/08',
+            'salary': '$114,367'
+        }, {
+            'name': 'Colette Foley',
+            'position': 'Lorem Inc.',
+            'office': 'Korea, North',
+            'ext': '8968',
+            'startDate': '2015/07/19',
+            'salary': '$721,473'
+        }, {
+            'name': 'Anastasia Shaffer',
+            'position': 'Dolor Nulla Semper LLC',
+            'office': 'Suriname',
+            'ext': '7980',
+            'startDate': '2015/04/20',
+            'salary': '$264,620'
+        }, {
+            'name': 'Gabriel Castro',
+            'position': 'Sed Limited',
+            'office': 'Bahrain',
+            'ext': '0757',
+            'startDate': '2015/03/04',
+            'salary': '$651,350'
+        }, {
+            'name': 'Shaine Forbes',
+            'position': 'Eu Arcu LLP',
+            'office': 'Cyprus',
+            'ext': '2369',
+            'startDate': '2015/01/18',
+            'salary': '$208,100'
+        }, {
+            'name': 'Alexandra Patrick',
+            'position': 'Ligula Donec Inc.',
+            'office': 'Viet Nam',
+            'ext': '8531',
+            'startDate': '2015/04/09',
+            'salary': '$104,063'
+        }, {
+            'name': 'Patience Vincent',
+            'position': 'Sem Molestie Associates',
+            'office': 'Philippines',
+            'ext': '8888',
+            'startDate': '2015/07/04',
+            'salary': '$673,556'
+        }, {
+            'name': 'Evelyn Smith',
+            'position': 'Fusce Industries',
+            'office': 'Togo',
+            'ext': '5051',
+            'startDate': '2015/08/15',
+            'salary': '$737,284'
+        }, {
+            'name': 'Kieran Gonzalez',
+            'position': 'Non Corp.',
+            'office': 'Equatorial Guinea',
+            'ext': '4834',
+            'startDate': '2015/08/24',
+            'salary': '$90,195'
+        }, {
+            'name': 'Molly Oneil',
+            'position': 'Non Dui Consulting',
+            'office': 'Belize',
+            'ext': '7501',
+            'startDate': '2014/10/28',
+            'salary': '$140,767'
+        }, {
+            'name': 'Nigel Davenport',
+            'position': 'Ullamcorper Velit In Industries',
+            'office': 'Vanuatu',
+            'ext': '0976',
+            'startDate': '2015/03/16',
+            'salary': '$70,536'
+        }, {
+            'name': 'Thor Young',
+            'position': 'Malesuada Consulting',
+            'office': 'French Southern Territories',
+            'ext': '0211',
+            'startDate': '2015/01/28',
+            'salary': '$75,501'
+        }, {
+            'name': 'Finn Delacruz',
+            'position': 'Lorem Industries',
+            'office': 'Cocos (Keeling) Islands',
+            'ext': '2980',
+            'startDate': '2014/12/11',
+            'salary': '$754,967'
+        }, {
+            'name': 'Lane Henderson',
+            'position': 'Pede Foundation',
+            'office': 'Kazakhstan',
+            'ext': '1446',
+            'startDate': '2015/07/02',
+            'salary': '$842,050'
+        }, {
+            'name': 'Shea Potter',
+            'position': 'Curabitur Limited',
+            'office': 'Timor-Leste',
+            'ext': '4654',
+            'startDate': '2015/05/07',
+            'salary': '$263,629'
+        }, {
+            'name': 'Brynn Yang',
+            'position': 'Ut Limited',
+            'office': 'Mayotte',
+            'ext': '4668',
+            'startDate': '2015/01/17',
+            'salary': '$74,292'
+        }, {
+            'name': 'Kylan Fuentes',
+            'position': 'Sapien Aenean Associates',
+            'office': 'Brazil',
+            'ext': '6623',
+            'startDate': '2014/12/28',
+            'salary': '$108,632'
+        }, {
+            'name': 'Lionel Mcbride',
+            'position': 'Ipsum PC',
+            'office': 'Portugal',
+            'ext': '3978',
+            'startDate': '2015/07/11',
+            'salary': '$34,244'
+        }, {
+            'name': 'Paul Lucas',
+            'position': 'Eget LLP',
+            'office': 'Nicaragua',
+            'ext': '8890',
+            'startDate': '2014/09/30',
+            'salary': '$690,834'
+        }, {
+            'name': 'Lareina Williamson',
+            'position': 'Imperdiet Ullamcorper Ltd',
+            'office': 'Cocos (Keeling) Islands',
+            'ext': '9489',
+            'startDate': '2014/12/01',
+            'salary': '$603,498'
+        }, {
+            'name': 'Amy Acevedo',
+            'position': 'Id Institute',
+            'office': 'Cook Islands',
+            'ext': '5592',
+            'startDate': '2015/02/04',
+            'salary': '$125,165'
+        }, {
+            'name': 'Nomlanga Silva',
+            'position': 'Eget LLC',
+            'office': 'Belize',
+            'ext': '3110',
+            'startDate': '2015/01/31',
+            'salary': '$268,509'
+        }, {
+            'name': 'Amena Stone',
+            'position': 'Enim Incorporated',
+            'office': 'Guinea',
+            'ext': '1211',
+            'startDate': '2014/09/23',
+            'salary': '$214,381'
+        }, {
+            'name': 'Danielle Coffey',
+            'position': 'Feugiat Placerat Corp.',
+            'office': 'Sao Tome and Principe',
+            'ext': '8176',
+            'startDate': '2015/06/17',
+            'salary': '$137,423'
+        }, {
+            'name': 'Buffy Russell',
+            'position': 'Lacus Quisque Ltd',
+            'office': 'Ecuador',
+            'ext': '6741',
+            'startDate': '2014/10/17',
+            'salary': '$612,184'
+        }, {
+            'name': 'Kaitlin Lamb',
+            'position': 'Malesuada Fringilla Est Associates',
+            'office': 'Algeria',
+            'ext': '5054',
+            'startDate': '2014/10/18',
+            'salary': '$327,367'
+        }, {
+            'name': 'Leilani Yates',
+            'position': 'Mus Proin LLC',
+            'office': 'South Sudan',
+            'ext': '1550',
+            'startDate': '2015/05/27',
+            'salary': '$743,493'
+        }, {
+            'name': 'Jemima Moon',
+            'position': 'Phasellus Corp.',
+            'office': 'South Georgia and The South Sandwich Islands',
+            'ext': '7582',
+            'startDate': '2015/05/21',
+            'salary': '$496,067'
+        }, {
+            'name': 'Hiroko Schwartz',
+            'position': 'Neque Institute',
+            'office': 'Saint Vincent and The Grenadines',
+            'ext': '9368',
+            'startDate': '2015/03/13',
+            'salary': '$178,782'
+        }, {
+            'name': 'Nathaniel Jensen',
+            'position': 'Mi Tempor Limited',
+            'office': 'Dominica',
+            'ext': '8331',
+            'startDate': '2014/12/05',
+            'salary': '$37,441'
+        }, {
+            'name': 'Silas Sweeney',
+            'position': 'Ultrices Institute',
+            'office': 'Turkmenistan',
+            'ext': '0746',
+            'startDate': '2014/11/13',
+            'salary': '$152,980'
+        }, {
+            'name': 'Jermaine Barry',
+            'position': 'Dapibus Corporation',
+            'office': 'Uzbekistan',
+            'ext': '1545',
+            'startDate': '2015/03/06',
+            'salary': '$409,463'
+        }, {
+            'name': 'Tatiana Nichols',
+            'position': 'Nec Diam Industries',
+            'office': 'Cook Islands',
+            'ext': '4395',
+            'startDate': '2015/05/22',
+            'salary': '$51,155'
+        }, {
+            'name': 'Rama Waller',
+            'position': 'Sem Pellentesque LLC',
+            'office': 'Andorra',
+            'ext': '2973',
+            'startDate': '2014/12/01',
+            'salary': '$223,227'
+        }];
 
-  public data:Array<any> = [{
-    'name': 'Victoria Cantrell',
-    'position': 'Integer Corporation',
-    'office': 'Croatia',
-    'ext': '0839',
-    'startDate': '2015/08/19',
-    'salary': '$208,178'
-  }, {
-    'name': 'Pearl Crosby',
-    'position': 'In PC',
-    'office': 'Cambodia',
-    'ext': '8262',
-    'startDate': '2014/10/08',
-    'salary': '$114,367'
-  }, {
-    'name': 'Colette Foley',
-    'position': 'Lorem Inc.',
-    'office': 'Korea, North',
-    'ext': '8968',
-    'startDate': '2015/07/19',
-    'salary': '$721,473'
-  }, {
-    'name': 'Anastasia Shaffer',
-    'position': 'Dolor Nulla Semper LLC',
-    'office': 'Suriname',
-    'ext': '7980',
-    'startDate': '2015/04/20',
-    'salary': '$264,620'
-  }, {
-    'name': 'Gabriel Castro',
-    'position': 'Sed Limited',
-    'office': 'Bahrain',
-    'ext': '0757',
-    'startDate': '2015/03/04',
-    'salary': '$651,350'
-  }];
-
-  constructor() {
-    this.length = this.data.length;
-  }
-
-  ngOnInit() {
-    this.onChangeTable(this.config, null);
-  }
-
-  changePage(page:any, data:Array<any> = this.data):Array<any> {
-    console.log(page);
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
-  }
-
-  changeSort(data:any, config:any) {
-    if (!config.sorting) {
-      return data;
+    constructor() {
+        this.length = this.data.length;
     }
 
-    // simple sorting
-    return data.sort((previous:any, current:any) => {
-      let columns = this.config.sorting.columns || [];
-      for (let i = 0; i < columns.length; i++) {
-        let columnName = columns[i].name;
+    ngOnInit() {
+        this.onChangeTable(this.config, null);
+    }
 
-        if (previous[columnName] > current[columnName]) {
-          return columns[i].sort === 'desc' ? -1 : 1;
+    changeSort(sortCol: any) {
+        console.log('hello sort', sortCol);
+
+        this.sortField = sortCol;
+        this.tableData.sort(function(a, b) {
+            console.log(b[sortCol], 'jjj', a[sortCol]);
+            return parseFloat(a[sortCol]) - parseFloat(b[sortCol]);
+        });
+
+        // simple sorting
+        //        return this.tableData.sort((previous: any, current: any) => {
+        //            let columns = sortCol || [];
+        //            for (let i = 0; i < this.tableData.length; i++) {
+        //                let columnName = this.tableData[i][columns];
+        //
+        //                if (previous[columnName] > current[columnName]) {
+        //                    return columns[i].sort === 'desc' ? -1 : 1;
+        //                }
+        //                if (previous[columnName] < current[columnName]) {
+        //                    return columns[i].sort === 'asc' ? -1 : 1;
+        //                }
+        //            }
+        //            return 0;
+        //        });
+    }
+
+
+    onChangeTable(config: any, page1: any = config.paging) {
+        console.log(config, page1, 'page change called', this.itemsPerPage);
+        if (page1 == null) {
+            page1 = 1;
         }
-        if (previous[columnName] < current[columnName]) {
-          return columns[i].sort === 'asc' ? -1 : 1;
+        else {
+            page1 = page1.page;
         }
-      }
-      return 0;
-    });
-  }
-
-  changeFilter(data:any, config:any):any {
-    if (!config.filtering) {
-      return data;
+        var begin = ((page1 - 1) * this.itemsPerPage),
+            end = begin + this.itemsPerPage;
+        var pageData = this.data.slice(begin, end);
+        console.log(pageData, 'data hereee', this.data);
+        //        this.tableData=[]; 
+        this.tableData = pageData;
+        console.log(this.tableData, 'in array')
     }
-
-    let filteredData:Array<any> = data.filter((item:any) =>
-      item[config.filtering.columnName].match(this.config.filtering.filterString));
-
-    return filteredData;
-  }
-
-  onChangeTable(config:any, page:any = config.paging) {
-    if (config.filtering) {
-      Object.assign(this.config.filtering, config.filtering);
-    }
-    if (config.sorting) {
-      Object.assign(this.config.sorting, config.sorting);
-    }
-
-    let filteredData = this.changeFilter(this.data, this.config);
-    let sortedData = this.changeSort(filteredData, this.config);
-    this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-    this.length = sortedData.length;
-  }
 }
 
-//@Component({
-//    selector: 'my-app',
-//    directive: [TableDemo],
-//    templateUrl: 'partials/wwref.html'
-//})
-//export class AppComponent {
-//}
-bootstrap(AppComponent,[]);
+bootstrap(AppComponent, []);
